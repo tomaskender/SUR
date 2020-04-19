4d9da2fde3f1cbd98004bb2ee53970c1e79246b1
Tomas Kender <xkende01@vutbr.cz>
2020-04-19 23:25:56 +0200

    system selector

--- src/detector.py
+++ src/detector.py
@@ -2,6 +2,7 @@ import os, glob, sys  # for managing directory paths and files
 import numpy as np
 from PIL import Image
 import face_detector as face
+import voice_detector as voice
 import argparse
 
 PARENT_DIR = os.getcwd()
@@ -16,6 +17,7 @@ parser.add_argument("--evalonly", action="store_true")
 parser.add_argument("--trainonly",  action="store_true")
 parser.add_argument("--verbose",  action="store_true")
 parser.add_argument("--plot",  action="store_true")
+parser.add_argument("--system")
 arguments = parser.parse_args()
 
 if arguments.evalonly and arguments.trainonly:
@@ -32,6 +34,8 @@ if arguments.verbose:
 def get_image_file(file):
 	return [np.array(Image.open(f).convert('RGB')) for f in glob.glob(file)]
 
+def get_audio_file(file):
+    return [read(f) for f in glob.glob(file)]
 
 # loads images and concatenates train and test data
 def image_data():
@@ -49,16 +53,34 @@ def image_data():
 
 	return train_x, train_y, test_x, test_y
 
+# loads voices and concatenates train and test data
+def voice_data():
+	test_x = np.array(get_voice_data(DATA_DIR + 'target_dev/*.wav'))
+	test_y = np.array([1] * len(test_x))  # 1 is our wanted voice
+	t = np.array(get_voice_data(DATA_DIR + 'non_target_dev/*.wav'))
+	test_y = np.concatenate((test_y, np.array([0] * len(t))), axis=0)
+	test_x = np.concatenate((test_x, t), axis=0)
 
-if not arguments.evalonly:
-	train_x, train_y, test_x, test_y = image_data()
-	face.build_model(train_x, train_y, test_x, test_y, USE_CNN=USE_CNN, PLOTS=PLOTS)
-if not arguments.trainonly:
-	face.evaluate(cnn=USE_CNN, verbose=VERBOSE)
+	train_x = np.array(get_voice_data(DATA_DIR + 'target_train/*.wav'))
+	train_y = np.array([1] * len(train_x))  # 1 is our wanted voice
+	t = np.array(get_voice_data(DATA_DIR + 'non_target_train/*.wav'))
+	train_y = np.concatenate((train_y, np.array([0] * len(t))), axis=0)
+	train_x = np.concatenate((train_x, t), axis=0)
 
+	return train_x, train_y, test_x, test_y
 
-def print_result(sample_name, loss, is_target):
-	"""Print result of sample analysis to stdout."""
-	print("{} {} {}\n".format(sample_name, loss, is_target))
-
-print("test", 1.0, True)
+if arguments.system == 'face':
+	if not arguments.evalonly:
+		train_x, train_y, test_x, test_y = image_data()
+		face.build_model(train_x, train_y, test_x, test_y, USE_CNN=USE_CNN, PLOTS=PLOTS)
+	if not arguments.trainonly:
+		face.evaluate(cnn=USE_CNN, verbose=VERBOSE)
+elif arguments.system == 'voice':
+	if not arguments.evalonly:
+		train_x, train_y, test_x, test_y = voice_data()
+		voice.build_model(train_x, train_y, test_x, test_y)
+	if not arguments.trainonly:
+		voice.evaluate(verbose=VERBOSE)
+else:
+	sys.stderr.write("ERROR: Invalid system argument!\n")
+	sys.exit(0)
---
+++ src/voice_detector.py
@@ -0,0 +1,22 @@
+import numpy as np
+#from sklearn.mixture import GMM
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import preprocessing
+from python_speech_features import mfcc
+
+def extract_features(audio, rate):
+    mfcc_feature = mfcc(audio,rate, winlen=0.025, winstep=0.01, numcep=20,nfft = 1200, appendEnergy = True)    
+    mfcc_feature = preprocessing.scale(mfcc_feature)
+    delta = calculate_delta(mfcc_feature)
+    combined = np.hstack((mfcc_feature,delta)) 
+    return combined
+
+def build_model(train_x, train_y, test_x, test_y):
+    #model = GMM(n_components=16, n_iter=500, convariance_type='diag')
+    model = RandomForestClassifier()
+    model.fit(train_x, train_y)
+    x_pred = model.predict(test_x)
+	#print("ACCURACY SCORE: ", accuracy_score(model.predict(test_x), test_y)))
+
+def evaluate(verbose = 0):
+    pass
